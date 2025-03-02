86ba3257f84afecbbbb71a7938b420feba418fbb
OtaraAlex <mbechealex61@gmail.com>
2025-03-02 15:37:22 +0300

    add sign-in and sign-up screens with form fields and Google login option

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,92 @@
+import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FormField from "@/components/FormField";
+import { Link } from "expo-router";
+import CustomButton from "@/components/CustomButton";
+import { useState } from "react";
+import { icons, images } from "@/constants";
+
+const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const handleSubmit = async () => {};
+  const handleGoogleLogin = async () => {};
+  return (
+    <SafeAreaView className="bg-white h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+
+          <Text className="text-2xl text-semibold mt-10 font-rubik-semibold">
+            Log in to ReadBlendr
+          </Text>
+
+          <TouchableOpacity
+            onPress={handleGoogleLogin}
+            className="bg-gray-100 shadow-md shadow-zinc-300 rounded-full w-full py-5 mt-7"
+          >
+            <View className="flex flex-row items-center justify-center">
+              <Image
+                source={icons.google}
+                className="w-5 h-5"
+                resizeMode="contain"
+              />
+              <Text className="text-lg font-rubik-medium text-black-300 ml-2">
+                Continue with Google
+              </Text>
+            </View>
+          </TouchableOpacity>
+
+          <View className="flex flex-row items-center mt-7">
+            <View className="flex-1 h-px bg-gray-200" />
+            <Text className="mx-4 text-gray-500 font-rubik-medium">or</Text>
+            <View className="flex-1 h-px bg-gray-200" />
+          </View>
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            title="Sign In"
+            handlePress={handleSubmit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="flex justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg font-rubik">New to ReadBlendr?</Text>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-primary-300"
+            >
+              Sign Up
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default SignIn;
